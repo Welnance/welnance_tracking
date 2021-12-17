00bab5ca3099a53cb027fe7fcd4ce055820d2e8f
Pho-86 <phothsack-tpt@welnance.com>
2021-12-17 11:33:31 +0700

    update CORS permission

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from routes.socials import socials
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(socials)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
